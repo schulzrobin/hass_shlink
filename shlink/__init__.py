60dbb908e1fa15a58722f68e5b8196738d8b7a8e
schulzrobin <106838867+schulzrobin@users.noreply.github.com>
2024-12-29 14:45:00 +0100

    Aktualisieren von __init__.py

--- shlink/__init__.py
+++ shlink/__init__.py
@@ -1,18 +1,22 @@
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 DOMAIN = "shlink"
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Shlink integration."""
-    hass.data[DOMAIN] = {}
+    hass.data.setdefault(DOMAIN, {})
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Shlink from a config entry."""
     hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Hier können Sensoren oder andere Plattformen initialisiert werden
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a Shlink config entry."""
     hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
