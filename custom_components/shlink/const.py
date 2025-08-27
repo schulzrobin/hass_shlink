74301f81be962d7a2962dac6676d7d4a7a09ec1a
Robin Schulz <106838867+schulzrobin@users.noreply.github.com>
2025-08-27 08:07:03 +0200

    const.py erstellen

---
+++ custom_components/shlink/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "shlink"
+
+CONF_BASE_URL = "base_url"
+CONF_API_KEY = "api_key"
+CONF_API_VERSION = "api_version"
+DEFAULT_API_VERSION = "3"  # funktioniert mit v3/v4 – Endpunkte sind stabil
+
+PLATFORMS = ["sensor"]
+
+COORDINATOR_UPDATE_INTERVAL = 300  # Sekunden
